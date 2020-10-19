cf110aee66b00a40ffcc2a8dc6a6a4c2276fd8df
Dwayne Risley <RisleyDwayne@gmail.com>
2020-10-19 14:37:12 -0400

    api route add

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,54 @@ const express = require("express");
 const router = express.Router();
 const db = ("../models");
 
-router.use("/workouts", apiRoutes);
+//get workouts
+router.get("/workouts", (req, res) => {
+    db.Workout.find({}).then(
+        (workout) => {
+            res.json(workout);
+        }).catch((err) => {
+            console.log(err);
+        });
+});
 
-module.exports = router;
+router.get("/workouts/range", (req, res) => {
+    db.Workout.find({}).then(
+        (workout) => {
+            res.json(workout);
+        }).catch((err) => {
+            console.log(err);
+        });
+});
+
+//create workout
+router.post("/workouts", (req, res) => {
+    db.Workout.create({})
+        .then((workout) => {
+            res.json(workout);
+        }).catch((err) => {
+            console.log(err);
+        });
+});
+
+router.post("/workouts/range", (req, res) => {
+    db.Workout.create({})
+        .then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            res.json(err);
+        })
+})
+
+router.put("/workouts/:id", ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
