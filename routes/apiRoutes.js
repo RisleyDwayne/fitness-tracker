99351fe31fdbaa2132f29985b9970b62df69bd8f
Dwayne Risley <RisleyDwayne@gmail.com>
2020-10-19 14:23:15 -0400

    tweaking router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const apiRoutes = require("./apiRoutes");
-
 const router = express.Router();
+const db = ("../models");
 
 router.use("/workouts", apiRoutes);
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes/index");
+const routes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
 
@@ -15,13 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
